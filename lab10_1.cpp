a681d629cc6b33d173762a647189e3c976f828e5
Siriphum <Jandeemanee0306@gmail.com>
2025-12-26 19:27:09 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,26 +2,51 @@
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
+int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	int studentNum = 1; 
+    char grade;
+
+
+	cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
+	while (true) {
+
+		cout << "Student [" << studentNum << "]: ";
+		cin >> grade; 
+		
+		if (grade == '0') {
+            break; 
+        }
+
+        if (grade == 'A') {
+            count[0]++;
+            studentNum++;
+        } else if (grade == 'B') {
+            count[1]++;
+            studentNum++;
+        } else if (grade == 'C') {
+            count[2]++;
+            studentNum++;
+        } else if (grade == 'D') {
+            count[3]++;
+            studentNum++;
+        } else if (grade == 'F') {
+            count[4]++;
+            studentNum++;
+        } else {
+            cout << "Wrong input. Please input again.\n";
+           
+        }
+    }
+
+    int total = count[0] + count[1] + count[2] + count[3] + count[4];
+
+    cout << "In total " << total << " students.\n";
+    cout << "A = " << count[0] << ", ";
+    cout << "B = " << count[1] << ", ";
+    cout << "C = " << count[2] << ", ";
+    cout << "D = " << count[3] << ", ";
+    cout << "F = " << count[4] << "\n";
 	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
 	
 	return 0;
 }
